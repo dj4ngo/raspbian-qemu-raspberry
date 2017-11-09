b465de92e96e8777ba9c899fdbd32c68e65f644f
Sebastien Boyron <sebastien@boyron.eu>
2017-11-09 22:24:25 +0100

    refactor code

--- create_env.sh
+++ create_env.sh
@@ -1,9 +1,65 @@
 #!/bin/bash -e
 
 dir="$(dirname $(readlink -f $0))"
+raspbian_url="http://downloads.raspberrypi.org/raspbian/images/"
+tmp_file="$(mktemp)"
 
-grep -q -- "-v[v]*" <<< "$1" && DEBUG="$1"
+# remove file on exit
+trap "{ rm -f $tmp_file; }" EXIT 
 
-sudo ansible-playbook -i "$dir/ansible/inventory" "$dir/ansible/setup.yml" $DEBUG
 
+# debug mode for ansible playbook
+grep -q -- "-v[v]*\>" <<< "${@}" && DEBUG="$1" && echo DEBUG
+
+
+function usage () {
+	cat << EOF
+REQUIREMENTS
+	Ansible is required to run this script
+SYNOPSIS	
+	${0##*/} get_raspbian_versions
+	${0##*/} setup raspbian_version (raspbian version is string returned by get_raspbian_versions)
+EOF
+}
+
+function get_raspbian_versions () {
+
+	curl -s $raspbian_url -o /dev/shm/tmp.html
+	echo "cat //html/body/table/tr/td/a/text()" | xmllint --html --shell /dev/shm/tmp.html | sed -n 's/\(.*raspbian.*\)\/$/\1/p'
+}
+
+
+function run_playbook () {
+	playbook="$1"
+	extra_vars="$2"
+	set -x
+	sudo ansible-playbook -i "$dir/ansible/inventory" --extra-vars "$extra_vars" "$dir/ansible/${playbook}.yml" $DEBUG
+	set +x
+}
+
+
+function setup () {
+	raspbian_version=$1
+
+	if [ -n "$raspbian_version" ]; then
+		run_playbook setup "raspbian_version=$raspbian_version"
+	else
+		usage
+	fi
+}
+
+### Main
+
+case $1 in
+	get_raspbian_versions)
+		get_raspbian_versions
+		;;
+	setup)
+		shift
+		setup ${@}
+		;;
+	*)
+		usage
+		;;
+esac
 
